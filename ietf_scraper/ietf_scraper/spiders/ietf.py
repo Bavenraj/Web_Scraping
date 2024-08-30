28cf2ec35a7455bbdbe5dcb25152b6b0fab9a656
BAVENRAJ <bavenraj08@gmail.com>
2024-08-30 11:50:28 +0800

    done

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import w3lib
 
 class IetfSpider(scrapy.Spider):
     name = "ietf"
@@ -8,7 +8,15 @@ class IetfSpider(scrapy.Spider):
 
     def parse(self, response):
         title1 = response.css('span.title::text').get()
-        title2 = response.xpath('//span[@class="title"]/text()').get()
+        title2 = response.xpath('//span[@class="subheading"]/text()').get()
        
-        return {"title1": title1,
-                "title2": title2}
+        return {"title1": response.css('span.title::text').get(),
+                "number": response.xpath('//span[@class="rfc-no"]/text()').get(),
+                "title2" : response.xpath('//span[@class="subheading"]/text()').get(),
+                "date": response.xpath('//span[@class="date"]/text()').get(),
+                "description": response.xpath('//meta[@name="DC.Description.Abstract"]/@content').get(),
+                'author': response.xpath('//meta[@name="DC.Creator"]/@content').get(),
+                'company': response.xpath('//span[@class="author-company"]/text()').get(),
+                'address': response.xpath('//span[@class="address"]/text()').get(),
+                'text':w3lib.html.remove_tags(response.xpath('//div[@class="text"]').get()),
+                'headings': response.xpath('//span[@class="subheading"]/text()').getall()}
