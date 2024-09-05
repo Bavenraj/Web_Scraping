643065de367c14089dbcd69d61a057c07940c969
BAVENRAJ <bavenraj08@gmail.com>
2024-09-05 11:35:06 +0800

    location

---
+++ location/AutocompletionService (1).js
@@ -0,0 +1,269 @@
+/**/_xdc_._bekm1o && _xdc_._bekm1o( {
+   "predictions" : 
+   [
+      {
+         "description" : "Shah Alam, Selangor, Malaysia",
+         "matched_substrings" : 
+         [
+            {
+               "length" : 4,
+               "offset" : 0
+            }
+         ],
+         "place_id" : "ChIJx70qk4tNzDERxZGwq4XgZW4",
+         "reference" : "ChIJx70qk4tNzDERxZGwq4XgZW4",
+         "structured_formatting" : 
+         {
+            "main_text" : "Shah Alam",
+            "main_text_matched_substrings" : 
+            [
+               {
+                  "length" : 4,
+                  "offset" : 0
+               }
+            ],
+            "secondary_text" : "Selangor, Malaysia"
+         },
+         "terms" : 
+         [
+            {
+               "offset" : 0,
+               "value" : "Shah Alam"
+            },
+            {
+               "offset" : 11,
+               "value" : "Selangor"
+            },
+            {
+               "offset" : 21,
+               "value" : "Malaysia"
+            }
+         ],
+         "types" : 
+         [
+            "locality",
+            "political",
+            "geocode"
+         ]
+      },
+      {
+         "description" : "Shah Alam Stadium, Jalan Akuatik 13/64, Seksyen 13, Shah Alam, Selangor, Malaysia",
+         "matched_substrings" : 
+         [
+            {
+               "length" : 4,
+               "offset" : 0
+            }
+         ],
+         "place_id" : "ChIJLcYgUZhNzDERSMa8YEhU99g",
+         "reference" : "ChIJLcYgUZhNzDERSMa8YEhU99g",
+         "structured_formatting" : 
+         {
+            "main_text" : "Shah Alam Stadium",
+            "main_text_matched_substrings" : 
+            [
+               {
+                  "length" : 4,
+                  "offset" : 0
+               }
+            ],
+            "secondary_text" : "Jalan Akuatik 13/64, Seksyen 13, Shah Alam, Selangor, Malaysia"
+         },
+         "terms" : 
+         [
+            {
+               "offset" : 0,
+               "value" : "Shah Alam Stadium"
+            },
+            {
+               "offset" : 19,
+               "value" : "Jalan Akuatik 13/64"
+            },
+            {
+               "offset" : 40,
+               "value" : "Seksyen 13"
+            },
+            {
+               "offset" : 52,
+               "value" : "Shah Alam"
+            },
+            {
+               "offset" : 63,
+               "value" : "Selangor"
+            },
+            {
+               "offset" : 73,
+               "value" : "Malaysia"
+            }
+         ],
+         "types" : 
+         [
+            "stadium",
+            "point_of_interest",
+            "establishment"
+         ]
+      },
+      {
+         "description" : "Shahab Perdana, Alor Setar, Kedah, Malaysia",
+         "matched_substrings" : 
+         [
+            {
+               "length" : 4,
+               "offset" : 0
+            }
+         ],
+         "place_id" : "ChIJA2UiClRbSzARaPKJ0pCaG9k",
+         "reference" : "ChIJA2UiClRbSzARaPKJ0pCaG9k",
+         "structured_formatting" : 
+         {
+            "main_text" : "Shahab Perdana",
+            "main_text_matched_substrings" : 
+            [
+               {
+                  "length" : 4,
+                  "offset" : 0
+               }
+            ],
+            "secondary_text" : "Alor Setar, Kedah, Malaysia"
+         },
+         "terms" : 
+         [
+            {
+               "offset" : 0,
+               "value" : "Shahab Perdana"
+            },
+            {
+               "offset" : 16,
+               "value" : "Alor Setar"
+            },
+            {
+               "offset" : 28,
+               "value" : "Kedah"
+            },
+            {
+               "offset" : 35,
+               "value" : "Malaysia"
+            }
+         ],
+         "types" : 
+         [
+            "sublocality_level_1",
+            "sublocality",
+            "political",
+            "geocode"
+         ]
+      },
+      {
+         "description" : "Shah Alam Hospital, Seksyen 7, Shah Alam, Selangor, Malaysia",
+         "matched_substrings" : 
+         [
+            {
+               "length" : 4,
+               "offset" : 0
+            }
+         ],
+         "place_id" : "ChIJV6R-SZ5TzDERZmkh60em73Q",
+         "reference" : "ChIJV6R-SZ5TzDERZmkh60em73Q",
+         "structured_formatting" : 
+         {
+            "main_text" : "Shah Alam Hospital",
+            "main_text_matched_substrings" : 
+            [
+               {
+                  "length" : 4,
+                  "offset" : 0
+               }
+            ],
+            "secondary_text" : "Seksyen 7, Shah Alam, Selangor, Malaysia"
+         },
+         "terms" : 
+         [
+            {
+               "offset" : 0,
+               "value" : "Shah Alam Hospital"
+            },
+            {
+               "offset" : 20,
+               "value" : "Seksyen 7"
+            },
+            {
+               "offset" : 31,
+               "value" : "Shah Alam"
+            },
+            {
+               "offset" : 42,
+               "value" : "Selangor"
+            },
+            {
+               "offset" : 52,
+               "value" : "Malaysia"
+            }
+         ],
+         "types" : 
+         [
+            "hospital",
+            "health",
+            "point_of_interest",
+            "establishment"
+         ]
+      },
+      {
+         "description" : "Shah Alam National Sports Complex Panasonic, Persiaran Perkilangan, Section 21, Shah Alam, Selangor, Malaysia",
+         "matched_substrings" : 
+         [
+            {
+               "length" : 4,
+               "offset" : 0
+            }
+         ],
+         "place_id" : "ChIJQcMCz2pNzDERP6uOMVkeHOQ",
+         "reference" : "ChIJQcMCz2pNzDERP6uOMVkeHOQ",
+         "structured_formatting" : 
+         {
+            "main_text" : "Shah Alam National Sports Complex Panasonic",
+            "main_text_matched_substrings" : 
+            [
+               {
+                  "length" : 4,
+                  "offset" : 0
+               }
+            ],
+            "secondary_text" : "Persiaran Perkilangan, Section 21, Shah Alam, Selangor, Malaysia"
+         },
+         "terms" : 
+         [
+            {
+               "offset" : 0,
+               "value" : "Shah Alam National Sports Complex Panasonic"
+            },
+            {
+               "offset" : 45,
+               "value" : "Persiaran Perkilangan"
+            },
+            {
+               "offset" : 68,
+               "value" : "Section 21"
+            },
+            {
+               "offset" : 80,
+               "value" : "Shah Alam"
+            },
+            {
+               "offset" : 91,
+               "value" : "Selangor"
+            },
+            {
+               "offset" : 101,
+               "value" : "Malaysia"
+            }
+         ],
+         "types" : 
+         [
+            "stadium",
+            "point_of_interest",
+            "establishment"
+         ]
+      }
+   ],
+   "status" : "OK"
+} )
