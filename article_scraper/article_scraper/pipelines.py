1d3aa489acdb475cdd90f01dc9f1b5a941e45152
BAVENRAJ <bavenraj08@gmail.com>
2024-10-02 21:20:57 +0800

    ls

--- article_scraper/article_scraper/pipelines.py
+++ article_scraper/article_scraper/pipelines.py
@@ -9,7 +9,10 @@ from scrapy.exceptions import DropItem
 from datetime import datetime
 
 class CheckItem:
- 
+    def process_item(self, article, spider):
+        if not article['lastUpdate'] or not article['url'] or not article['title']:
+            raise DropItem("Missing Something!")
+        return article
     
 class CheckDate:
     def process_item(self,article, spider):
