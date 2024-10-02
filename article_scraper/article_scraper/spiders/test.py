bf220b85063b56e04965a268a3d5dd74af8e9da0
BAVENRAJ <bavenraj08@gmail.com>
2024-10-02 21:19:50 +0800

    ls

--- article_scraper/article_scraper/spiders/test.py
+++ article_scraper/article_scraper/spiders/test.py
@@ -10,4 +10,13 @@ class IetfSpider(scrapy.Spider):
         title1 = response.css('span.title::text').get()
         title2 = response.xpath('//span[@class="subheading"]/text()').get()
        
-
+        return {"title1": response.css('span.title::text').get(),
+                "number": response.xpath('//span[@class="rfc-no"]/text()').get(),
+                "title2" : response.xpath('//span[@class="subheading"]/text()').get(),
+                "date": response.xpath('//span[@class="date"]/text()').get(),
+                "description": response.xpath('//meta[@name="DC.Description.Abstract"]/@content').get(),
+                'author': response.xpath('//meta[@name="DC.Creator"]/@content').get(),
+                'company': response.xpath('//span[@class="author-company"]/text()').get(),
+                'address': response.xpath('//span[@class="address"]/text()').get(),
+                'text':w3lib.html.remove_tags(response.xpath('//div[@class="text"]').get()),
+                'headings': response.xpath('//span[@class="subheading"]/text()').getall()}
