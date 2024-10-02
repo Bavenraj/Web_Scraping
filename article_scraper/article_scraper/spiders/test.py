ec72d260fb96fb265105e14ec544fd88486535b2
BAVENRAJ <bavenraj08@gmail.com>
2024-10-02 21:20:05 +0800

    ls

--- article_scraper/article_scraper/spiders/test.py
+++ article_scraper/article_scraper/spiders/test.py
@@ -2,9 +2,7 @@ import scrapy
 import w3lib
 
 class IetfSpider(scrapy.Spider):
-    name = "ietf"
-    allowed_domains = ["pythonscraping.com"]
-    start_urls = ["https://pythonscraping.com/linkedin/ietf.html"]
+
 
     def parse(self, response):
         title1 = response.css('span.title::text').get()
