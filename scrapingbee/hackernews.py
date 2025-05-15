7d0346e631e3d32c310a64e0c80483a28069f5ee
Bavenraj <bavenraj08@gmail.com>
2025-05-15 22:54:42 +0800

    scrapingbee

--- scrapingbee/hackernews.py
+++ scrapingbee/hackernews.py
@@ -1,5 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://news.ycombinator.com')
-print(r.status_code)
+#r = requests.get('https://news.ycombinator.com')
+#print(r.status_code)
+
+
+BASE_URL = 'https://news.ycombinator.com'
+USERNAME = "admin101"
+PASSWORD = "admin101"
+
+s = requests.Session()
+
+data = { "acct": USERNAME, "pw": PASSWORD}
+r = s.post(f'{BASE_URL}/login', data=data)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+if soup.find(id='logout') is not None:
+    print('Successfully logged in')
+else:
+    print('Authentication Error')
