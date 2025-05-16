9ca51c5346ff3707cd31b9cd781f862ab3b049c4
Bavenraj <bavenraj08@gmail.com>
2025-05-16 22:46:39 +0800

    scrapingbee

--- scrapingbee/hackernews.py
+++ scrapingbee/hackernews.py
@@ -1,24 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://news.ycombinator.com')
-#print(r.status_code)
-soup = BeautifulSoup(r.text, 'html.parser')
-links = soup.findAll('tr', class_='athing')
-
-formatted_links = []
-
-for link in links:
-    data = {
-        'id': link['id'],
-        'title': link.find_all('td')[2].a.text,
-        "url": link.find_all('td')[2].a['href'],
-        "rank": int(link.find_all('td')[0].span.text.replace('.', ''))
-    }
-    formatted_links.append(data)
-
-print(formatted_links)
-
 """
 BASE_URL = 'https://news.ycombinator.com'
 USERNAME = "admin101"
@@ -36,3 +18,35 @@ else:
     print('Authentication Error')
     
 """
+
+r = requests.get('https://news.ycombinator.com')
+#print(r.status_code)
+soup = BeautifulSoup(r.text, 'html.parser')
+links = soup.findAll('tr', class_='athing')
+
+formatted_links = []
+
+for link in links:
+    data = {
+        'id': link['id'],
+        'title': link.find_all('td')[2].a.text,
+        "url": link.find_all('td')[2].a['href'],
+        "rank": int(link.find_all('td')[0].span.text.replace('.', ''))
+    }
+    formatted_links.append(data)
+
+#print(formatted_links)
+
+import csv
+
+
+csv_file = 'hacker_news_posts.csv'
+
+with open(csv_file, 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['id', 'title', 'url', 'rank'])
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
+
+print("File Loaded")
+        
