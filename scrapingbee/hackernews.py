ad3e6293dcbcab46af2806ad5fbc9dc7bd085626
Bavenraj <bavenraj08@gmail.com>
2025-05-18 14:12:48 +0800

    scrapingbee

--- scrapingbee/hackernews.py
+++ scrapingbee/hackernews.py
@@ -24,16 +24,16 @@ r = requests.get('https://news.ycombinator.com')
 soup = BeautifulSoup(r.text, 'html.parser')
 links = soup.findAll('tr', class_='athing')
 
-formatted_links = []
+data = []
 
 for link in links:
-    data = {
+    data_link = {
         'id': link['id'],
         'title': link.find_all('td')[2].a.text,
         "url": link.find_all('td')[2].a['href'],
         "rank": int(link.find_all('td')[0].span.text.replace('.', ''))
     }
-    formatted_links.append(data)
+    data.append(data_link)
 
 #print(formatted_links)
 
