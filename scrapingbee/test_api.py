17249e2a3f8613e7fa28c0dde92f1f600379c12c
Bavenraj <bavenraj08@gmail.com>
2025-05-27 22:54:48 +0800

    api

---
+++ scrapingbee/test_api.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://api.example.com/data"
+headers = {
+    "Authorization": "Bearer your_token"
+}
+params = {
+    "param": "value"
+}
+
+response = requests.get(url, headers=headers, params=params)
+data = response.json()
+print(data)
