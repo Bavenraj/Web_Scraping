f0f139bb6dd36b9007aa29bbaf1658897b93bb13
Bavenraj <bavenraj08@gmail.com>
2025-06-04 22:59:29 +0800

    documentation tutorial

--- selenium_scraper/docs01.py
+++ selenium_scraper/docs01.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 driver = webdriver.Chrome()
 
-driver.get("http://selenium.dev")
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+#driver.implicitly_wait(1)
 
-driver.quit()
+title = driver.title
+text_box = driver.find_element(by = By.NAME, value="my-text")
+submit_button = driver.find_element(by = By.CSS_SELECTOR, value="button")
+text_box.send_keys("Selenium")
+submit_button.click()
+print(title)
+
+#driver.quit()
