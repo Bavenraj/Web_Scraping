be8229fe1ccd8b15484c23c9ddea50e2b0f960ff
Bavenraj <bavenraj08@gmail.com>
2025-06-07 23:24:07 +0800

    get_default_chrome_options()

--- selenium_scraper/docs01.py
+++ selenium_scraper/docs01.py
@@ -5,24 +5,35 @@ from selenium.webdriver.chrome.options import Options
 
 import logging
 
-# Set up logging to troubleshoot if anything goes wrong
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.info("Initializing WebDriver")
-options = Options()
 
 def get_default_chrome_options():
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
     return options
 
-options = get_default_chrome_options()
+# Set up logging to troubleshoot if anything goes wrong
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info("Initializing WebDriver")
+options = Options()
+#options = get_default_chrome_options()
+options.platform_name = 'any'
 options.browser_version = 'stable'
 options.page_load_strategy = 'eager'
 options.accept_insecure_certs = True
 driver = webdriver.Chrome(options=options)
 
+def test_bad_ssl():
+    options.accept_insecure_certs = True
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://expired.badssl.com/")
+    title = driver.title
+    print(title)
+    
+test_bad_ssl()
+
 logging.info("Loading Selenium Documentations")
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
 #driver.implicitly_wait(1)
 
 #title = driver.title
@@ -40,6 +51,8 @@ logging.info("Locating and printing success message")
 message = driver.find_element(by=By.ID, value="message")
 text = message.text
 print(text)
+
+
 
 logging.info("Closing WebDriver")
 driver.quit()
