c2adc1b84f7012c40160e0f98890b425037230ae
Bavenraj <bavenraj08@gmail.com>
2025-06-09 21:30:44 +0800

    shopee scraper

--- selenium_scraper/scraping_test.py
+++ selenium_scraper/scraping_test.py
@@ -1,18 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from selenium import webdriver
+import time
 
-r = requests.get('https://shopee.com.my/Mobile-Accessories-cat.11000979')
+#r = requests.get('https://shopee.com.my/Mobile-Accessories-cat.11000979')
 #print(r.status_code)
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')  # run in background
+options.page_load_strategy = 'eager'
+driver = webdriver.Chrome(options=options)
 
-soup = BeautifulSoup(r.text, 'html.parser')
+url = 'https://shopee.com.my/Mobile-Accessories-cat.11000979'
+driver.get(url)
+
+#time.sleep(5) 
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 product_list = soup.findAll('li', class_='shopee-search-item-result__item')
-
+print(product_list)
 product_data = []
 
 for product in product_list:
     product_chars = {
-        'product_name' :  product.find('img')['alt'],
+        'product_name' : product.find('img')['alt'],
         'product_link' : product.find('a', class_='contents')['href']       
     }
     product_data.append(product_chars)
