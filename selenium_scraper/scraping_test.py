fa1a03088bb55777310d31f9637646f6ba408d9d
Bavenraj <bavenraj08@gmail.com>
2025-06-10 22:46:29 +0800

    remove selenium

--- selenium_scraper/scraping_test.py
+++ selenium_scraper/scraping_test.py
@@ -16,9 +16,8 @@ r = requests.get('https://shopee.com.my/Mobile-Accessories-cat.11000979')
 #driver.get(url)
 
 #time.sleep(5) 
-print(r.encoding("utf-8"))
 soup = BeautifulSoup(r.text, 'html.parser')
-print(r.encoding("utf-8"))
+print(r.text)
 product_list = soup.find_all("link")#, classmethod='shopee-search-item-result__item')
 #print(product_list)
 product_data = []
