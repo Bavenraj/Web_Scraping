0e0d6a2e5ff1f008b3bf47fdd947910e4ee2ff0e
Bavenraj <bavenraj08@gmail.com>
2025-06-10 22:33:49 +0800

    remove selenium

--- selenium_scraper/scraping_test.py
+++ selenium_scraper/scraping_test.py
@@ -1,31 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-from selenium import webdriver
 import time
-from selenium.webdriver.common.by import By
 
 
-#r = requests.get('https://shopee.com.my/Mobile-Accessories-cat.11000979')
+r = requests.get('https://shopee.com.my/Mobile-Accessories-cat.11000979')
 #print(r.status_code)
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # run in background
-options.page_load_strategy = 'eager'
-driver = webdriver.Chrome(options=options)
+#options = webdriver.ChromeOptions()
+#options.add_argument('--headless')  # run in background
+#options.page_load_strategy = 'eager'
+#driver = webdriver.Chrome(options=options)
 
-url = 'https://shopee.com.my/Mobile-Accessories-cat.11000979'
-driver.get(url)
+#url = 'https://shopee.com.my/Mobile-Accessories-cat.11000979'
+#driver.get(url)
 
 #time.sleep(5) 
-
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-product_list = soup.findAll('li', class_='shopee-search-item-result__item')
+#print(r.prettify())
+soup = BeautifulSoup(r.text, 'html.parser')
+product_list = soup.find_all("link")#, classmethod='shopee-search-item-result__item')
 print(product_list)
 product_data = []
 
 for product in product_list:
     product_chars = {
-        'product_name' : driver.find_element(by = By.CLASS_NAME, value="contents")['href'],
+        'product_name' : product.find('a', class_='contents')['href'],
         'product_link' : product.find('a', class_='contents')['href']       
     }
     product_data.append(product_chars)
