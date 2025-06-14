5b406c31d3ce7fdd88e44e035b51cf56e2a1e874
Bavenraj <bavenraj08@gmail.com>
2025-06-14 14:11:28 +0800

    infinite scroll

--- selenium_scraper/geolocator.py
+++ selenium_scraper/geolocator.py
@@ -24,21 +24,33 @@ input = driver.find_element(by = By.CLASS_NAME, value = "searchboxinput")
 input.clear()
 
 logging.info("Searching for location")
-input.send_keys("kfc selangor", Keys.ENTER)
+query = 'kfc sarawak'
+input.send_keys(query, Keys.ENTER)
 time.sleep(5)
 
-scrollableElement = driver.find_element(by=By.CSS_SELECTOR, value =".kA9KIf")
-last_height = 0
-
+logging.info("Looking for scrollable element")
+scrollableElement = driver.find_element(by=By.CSS_SELECTOR, value =f"[aria-label='Results for {query}']")
+initial_count = 0
 while True:
-    driver.execute_script('arguments[0].scrollTop+=10000;', scrollableElement)
+    logging.info("Scrolling element")
+    for _ in range(3):
+        driver.execute_script('arguments[0].scrollBy(0,1000);', scrollableElement)
+        time.sleep(1)
+    
+    logging.info("Looking for result")
+    final_count = len(driver.find_elements(by=By.CLASS_NAME, value = "hfpxzc"))
+    print(str(initial_count)+" - "+ str(final_count))
     time.sleep(2)
     
-    new_height = driver.execute_script('return document.body.scrollHeight')
-    print(str(new_height) + " - " + str(last_height))
-    if(new_height==last_height):
+    if(initial_count!=final_count):
+        initial_count = final_count
+    else:
+        break
+    
+    #last_height = new_height
+    '''if(scrollableElement[-1]):
         break
     else:
         last_height = new_height
-    
+    '''
 
