ced51d97bb1a1dc45193ba5e3ea78b36bd4aa365
Bavenraj <bavenraj08@gmail.com>
2025-07-01 11:49:48 +0800

    Scrape & Extraction success

--- fast_food_scraper/KFC_Scrapper_2.py
+++ fast_food_scraper/KFC_Scrapper_2.py
@@ -46,7 +46,7 @@ def find_state(query):
     return driver
 
 def find_nearby_location(driver, query):    
-
+    logging.info("Getting nearby location input search box ")
     WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, "searchboxinput")))
     input = driver.find_element(by=By.ID, value = "searchboxinput")
     input.clear()
@@ -58,34 +58,38 @@ def find_nearby_location(driver, query):
     time.sleep(3)
     
     try: 
-        WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, f"[aria-label='Results for {query}']")))
+        logging.info("Looking for Searched Results")
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, f"[aria-label='Results for {query}']")))
         scrollableElement = driver.find_element(by=By.CSS_SELECTOR, value =f"[aria-label='Results for {query}']")
         initial_count = 0
-        logging.info("Found. Scrolling search results")
+        logging.info("Found. Scrolling search results list")
         while True:
             for _ in range(3):
                 driver.execute_script('arguments[0].scrollBy(0,1000);', scrollableElement)
                 time.sleep(1)
             
             final_count = len(driver.find_elements(by=By.CLASS_NAME, value = "hfpxzc"))
-            #print(str(initial_count)+" - "+ str(final_count))
             time.sleep(2)
             
             if(initial_count!=final_count):
                 initial_count = final_count
             else:
+                
+                logging.info("Extracting Page Source")
                 pageSource = driver.page_source
                 with open(f"pageSource_3/{query}.html", "w", encoding="utf-8") as file:
                     file.write(pageSource)
+                logging.info(f"{query}: {final_count}")
                 return final_count
     except:
-        logging.info("Element Not found. Only One Available Result")
+        logging.info("Result list not found. Only One Result available")
         time.sleep(2)
         logging.info("Extracting Page Source")
         pageSource = driver.page_source
         with open(f"pageSource_3/{query}.html", "w", encoding="utf-8") as file:
             file.write(pageSource)
         final_count = 1
+        logging.info(f"{query}: {final_count}")
         return final_count
            
 count = []
@@ -100,27 +104,30 @@ def start_scrape(state_list = mydict):
     #print(filtered_dict)
             
     for state, areas in filtered_dict.items():
-        load_map()
-        driverr = find_state(state)
         for area in areas:
+            start_time = time.perf_counter()
+            load_map()
+            driverr = find_state(state)
             query = f"KFC near {area}, {state}"
             count.append(find_nearby_location(driver=driverr, query = query))
+            end_time = time.perf_counter()
+            scraping_duration = end_time - start_time
+
             data_link = {
                 'State': state,
                 'Area' : area,
-                'Count': count[-1]
+                'Count': count[-1],
+                'Duration': round(scraping_duration, 2)
             }
             data.append(data_link) 
-        logging.info("Populating data into excel")
+            
         with open(csv_file, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['State','Area', 'Count'])
+            writer = csv.DictWriter(file, fieldnames=['State','Area', 'Count', 'Duration'])
             writer.writeheader()
             for row in data:
                 writer.writerow(row)
  
-        logging.info(f"Data for {query} was loaded into csv")
+        logging.info(f"Data for {state} was loaded into csv")
     print(data)
 
 start_scrape()
-
-
--- fast_food_scraper/data_extract.py
+++ fast_food_scraper/data_extract.py
@@ -3,7 +3,7 @@ from data_transform import mydict
 import csv
 import logging
 
-csv_file = 'data/kfc_data_4.csv'
+csv_file = 'data/kfc_data_3_1.csv'
 html_page_sources = []
 logging.basicConfig(#filename="kfcscrapper.log", encoding="utf-8", filemode="a",
                     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -17,59 +17,68 @@ def extract_source(state_list = mydict):
     #print(filtered_dict)
     for state, areas in filtered_dict.items():
         for area in areas:
-            query = f"pageSource_3\KFC near {area}, {state}.html"
-            html_page_sources.append(query)
-    return html_page_sources
+            extract_data(state, area)
+            #query = f"pageSource_3\KFC near {area}, {state}.html"
+           # html_page_sources.append(query)
+    #return html_page_sources
 
 store_data = []
-'''h1 -> store Name
-jANrlb  -> store rating
-HHrUdb or F7nice  -> store review'''
-def extract_data(page_sources):
-    for page_source in page_sources:
-        logging.info(f'Extracting data from {page_source}')
-        file = open(page_source)
-        soup = BeautifulSoup(open(page_source, encoding='utf-8').read(), 'html.parser')
-        if soup.find(name = 'a', attrs={"class": "hfpxzc"}):      
-            classes = soup.find_all(name='div', attrs={"class": "fontBodyMedium"})
-            for store in classes:
-                name = store.find(name= 'div', attrs={"class": "fontHeadlineSmall"})
+def extract_data(state, area):
+    
+    page_source = f"pageSource_3\KFC near {area}, {state}.html"
+    logging.info(f'Extracting data from {page_source}')
+    file = open(page_source)
+    soup = BeautifulSoup(open(page_source, encoding='utf-8').read(), 'html.parser')
+    if soup.find(name = 'a', attrs={"class": "hfpxzc"}):      
+        classes = soup.find_all(name='div', attrs={"class": "fontBodyMedium"})
+        for store in classes:
+            name = store.find(name= 'div', attrs={"class": "fontHeadlineSmall"})
+            if name is not None:
                 review_available = store.find(name= 'span', attrs={"class": "fontBodyMedium"})
                 if review_available is not None:
                     if review_available.text == "No reviews":
                         ratings = 0
                         review = 0
                     else:
-                        ratings = store.find(name= 'span', attrs={"class": "MW4etd"})
-                        review = store.find(name= 'span', attrs={"class": "UY7F9"})
-                        if name is not None:
-                            store_data.append({
-                                'Store Name' : name.text,
-                                'Rating' : ratings.text,
-                                'Review Count': review.text
-                            })
+                        ratings = store.find(name= 'span', attrs={"class": "MW4etd"}).text
+                        review = store.find(name= 'span', attrs={"class": "UY7F9"}).text
             
-        else: 
-            name = soup.find(name= "h1")
-            #if soup.find(name= 'div', attrs={"class": "F7nice"}) is no
-            ratings = soup.find(name= 'div', attrs={"class": "F7nice"})
-            review = soup.find(name= 'div', attrs={"class": "F7nice"})
-            #review._last_descendant().text
-            if name is not None:
-                store_data.append({
-                    'Store Name' : name.text,
-                    'Rating' : ratings.find_next().text,
-                    'Review Count': review._last_descendant().text
-                })
+                    store_data.append({
+                        'State': state,
+                        'Area' : area,
+                        'Store Name' : name.text,
+                        'Rating' : ratings,
+                        'Review Count': review
+                    })
+                else: 
+                    pass
+        
+    else: 
+        name = soup.find(name= "h1")
+        if name is not None:
+            if soup.find(name= 'div', attrs={"class": "dmRWX", "style": "display: none"}):
+                ratings = 0
+                review = 0
+            else:
+                ratings = soup.find(name= 'div', attrs={"class": "F7nice"}).find_next().text
+                review = soup.find(name= 'div', attrs={"class": "F7nice"})._last_descendant().text
+                
+            store_data.append({
+                'State': state,
+                'Area' : area,
+                'Store Name' : name.text,
+                'Rating' : ratings,
+                'Review Count': review
+            })
 
     with open(csv_file, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['Store Name','Rating', 'Review Count'])
+        writer = csv.DictWriter(file, fieldnames=['State', 'Area', 'Store Name','Rating', 'Review Count'])
         writer.writeheader()
         for row in store_data:
             writer.writerow(row)
     #print(store_data) 
 
 #print(extract_source(["W.P. Labuan", "W.P. Putrajaya"]))
-extract_data(extract_source())#["W.P. Kuala Lumpur"]))
+extract_source()#["W.P. Kuala Lumpur"]))
 
 #print(html_page_sources[221])
